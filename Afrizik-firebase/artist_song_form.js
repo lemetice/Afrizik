97a300300528c695b8a352626a10733774ca300a
lemetice <arnoldtagne@gmail.com>
2015-05-25 14:41:52 +0100

    deploying the afrizik web app
    Deploying the last modification adding on firebase web app.

---
+++ Afrizik-firebase/artist_song_form.js
@@ -0,0 +1,18 @@
+/**/
+
+var a_fb = new Firebase('https://afrizik.firebaseio.com/afrizik');
+
+angular.module('afrizik', [])
+
+.controller('ArtistSongCtrl', function($scope){
+	$scope.artist = {};
+
+	$scope.addSong = function(){
+		console.log($scope.artist);
+		var newArtist= $scope.artist;
+
+		var artistRef = a_fb.child("artists");
+		  artistRef.push(newArtist);
+	}
+})
+;
